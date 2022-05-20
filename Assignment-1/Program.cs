28e88b7b9d2853ee65f8f1c4667d128ab6ada117
aishwarya.nalawade <aishwarya.nalawade@AZ-VDI-D1-249>
2022-05-20 02:04:48 -0700

    Add project files.

---
+++ Assignment-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static int Power (int x,int y)
+        {
+            int temp;
+            if (y == 0)
+                return 1;
+            temp = Power(x, y / 2);
+            if (y % 2 == 0)
+                return temp * temp;
+            else
+                return x * temp * temp;
+        }
+    }
+}
